4114102b75739e5b70d15f4b3a09959ea8d8879a
aliSoltani-prog <alisoltaniprog@gmail.com>
2025-01-17 21:56:50 +0330

    week 1 project

---
+++ python/week 1/proj_2.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter the first number : "))
+num2 = int(input("enter the second number : "))
+print (f"{num1} + {num2} = {num1+num2}")
+print (f"{num1} - {num2} = {num1-num2}")
+print (f"{num1} / {num2} = {round(num1/num2)}")
+print (f"{num1} * {num2} = {num1*num2}")
---
+++ python/week 1/proj_3.py
@@ -0,0 +1,3 @@
+birth_year = int(input("enter your birth year (solar Hijri calendar): "))
+this_year = int(input("what is this year RN (solar Hijri calendar): "))
+print (f"you are {this_year-birth_year} years old")
