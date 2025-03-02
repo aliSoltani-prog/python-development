52eb685732a3e86f08edc8cf789259f12db2a34f
aliSoltani-prog <alisoltaniprog@gmail.com>
2025-03-02 08:53:59 +0330

    complete the calculator

---
+++ python/week 5/calculator/main.py
@@ -0,0 +1,81 @@
+import opratroes
+
+
+
+
+
+while True:
+    print("1.additional")
+    print("2.")
+    print("3.")
+    print("4.")
+    print("5.radical")
+    print("6.Cotangent")
+    print("7.Tangent")
+    print("8.Sinos")
+    print("9.Cosinos")
+    print("E.exit")
+    menu = print("what do you want to do  : ")
+    match menu:
+        case "1" :
+            try:
+                first = int(input("Enter the first number : "))
+                second = int(input("Enter the second number : "))
+                opratroes.additional(first=first , second=second)
+            except :
+                print("invalid input")
+        case "2" : 
+            try:
+                first = int(input("Enter the first number : "))
+                second = int(input("Enter the second number : "))
+                opratroes.Subtraction(first=first , second=second)
+            except:
+                print("invalid input")
+        case "3" : 
+            try:
+                first = int(input("Enter the first number : "))
+                second = int(input("Enter the second number : "))
+                opratroes.Multiplication(first=first , second=second)
+            except:
+                print("invalid input")
+        case "4" :
+            try:
+                first = int(input("Enter the first number : "))
+                second = int(input("Enter the second number : "))
+                opratroes.Division(first=first , second=second)
+            except:
+                print("invalid input")
+        case "5" :
+            try:
+                num = int(input("enter the number you want to see it's root"))
+                opratroes.radical(num=num)
+            except:
+                print("invalid input")
+        case "6" :
+            try:
+                num = int(input("enter the number you want to see it's sinos"))
+                opratroes.sin(num=num)
+            except:
+                print("invalid input")
+        case "7" :
+            try:
+                num = int(input("enter the number you want to see it's cosinos"))
+                opratroes.cos(num=num)
+            except:
+                print("invalid input")
+        case "8" :
+            try:
+                num = int(input("enter the number you want to see it's tangent"))
+                opratroes.tan(num=num)
+            except:
+                print("invalid input")
+        case "9" :
+            try:
+                num = int(input("enter the number you want to see it's ctangent"))
+                opratroes.cot(num=num)
+            except:
+                print("invalid input")
+        case "E" :
+            break
+        case _:
+            print("wrong answer. try again.")
---
+++ python/week 5/calculator/opratroes.py
@@ -0,0 +1,30 @@
+import math
+
+
+
+def additional(first , second):
+    print(f"{first} + {second} = {first+second}")
+
+def Subtraction(first , second):
+    print(f"{first} - {second} = {first-second}")
+
+def Multiplication(first , second):
+    print(f"{first} * {second} = {first*second}")
+
+def Division(first,second):
+    print(f"{first} / {second} = {first/second}")
+
+def radical(num):
+    print(f"the root of the {num} is {num**0/5}")
+
+def sin(num):
+    print(f"the sinos of the {num} is {math.sin(math.radians(num))}")
+
+def cos(num):
+    print(f"the cosinos of the {num} is {math.cos(math.radians(num))}")
+
+def tan(num):
+    print(f"the Tangent of the {num} is {math.tan(math.radians(num))}")
+
+def cot(num):
+    print(f"the Cotangent of the {num} is {1/math.tan(math.radians(num))}")
